d7daadea14f4281d9b5930b96660193593d2f3c2
Jackie Chen <support@jackiechen.org>
2015-11-09 12:23:17 +1100

    allow user to invalidate either staging or production

--- scripts/akamai.js
+++ scripts/akamai.js
@@ -22,7 +22,8 @@ function sendHelp(msg){
   helpString += "'akamai translate arl <hostname>' - Translates an Akamaized URL into typecode, origin server, CP code, serial number, TTL.\n";
   helpString += "'akamai locate ip <ipaddress >' - Retrieves the location details for the provided IP address.\n";
   helpString += "'akamai queue length' - Returns the number of outstanding objects in the user's queue.\n";
-  helpString += "'akamai purge content <url>' - Submits a request to purge Edge content represented by URL.\n";
+  helpString += "'akamai purge staging <url>' - Submits a request to purge staging Edge content represented by URL.\n";
+  helpString += "'akamai purge production <url>' - Submits a request to purge production Edge content represented by URL.\n";
   helpString += "'akamai purge status <purgeid>' -  Request that status information of the purge id.";
   msg.send(helpString);
 }
@@ -33,19 +34,19 @@ module.exports = function(robot){
   });
 
   robot.respond(/akamai translate error( Reference #| #|\s)(.*)/i, function(msg){
-    errorCode = msg.match[2];
+    errorCode = msg.match[2].replace(/ /g,'');
     msg.reply("Translating error code: " + errorCode);
     akamai.translateError(diag_client_token, diag_client_secret, diag_access_token, diag_url, errorCode, msg);
   });
 
   robot.respond(/akamai translate arl (.*)/i, function(msg){
-    hostName = msg.match[1];
+    hostName = msg.match[1].replace(/ /g,'');
     msg.reply("Translating Akamaized URL: " + hostName);
     akamai.translateArl(diag_client_token, diag_client_secret, diag_access_token, diag_url, hostName, msg);
   });
 
   robot.respond(/akamai locate ip (.*)/i, function(msg){
-    ipAddress = msg.match[1];
+    ipAddress = msg.match[1].replace(/ /g,'');
     msg.reply("Finding the geolocation of IP address: " + ipAddress);
     akamai.ipGeolocator(diag_client_token, diag_client_secret, diag_access_token, diag_url, ipAddress, msg);
   });
@@ -55,14 +56,15 @@ module.exports = function(robot){
     akamai.queueLength(ccu_client_token, ccu_client_secret, ccu_access_token, ccu_url, msg);
   });
 
-  robot.respond(/akamai purge content (.*)/i, function(msg){
-    page = msg.match[1];
-    msg.reply("Purging the content of: " + page);
-    akamai.purgeRequest(ccu_client_token, ccu_client_secret, ccu_access_token, ccu_url, page, msg);
+  robot.respond(/akamai purge (staging|production) (.*)/i, function(msg){
+    env = msg.match[1].replace(/ /g,'');
+    page = msg.match[2].replace(/ /g,'');
+    msg.reply("Purging the content of: " + page + " in " + env);
+    akamai.purgeRequest(ccu_client_token, ccu_client_secret, ccu_access_token, ccu_url, env, page, msg);
   });
 
   robot.respond(/akamai purge status (.*)/i, function(msg){
-    id = msg.match[1];
+    id = msg.match[1].replace(/ /g,'');
     msg.reply("Chekcing purge status of: " + id);
     akamai.purgeStatus(ccu_client_token, ccu_client_secret, ccu_access_token, ccu_url, id, msg);
   });
